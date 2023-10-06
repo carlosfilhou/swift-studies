8f24b0386a60992b95041870d32b7218f0d10170
Carlos Augusto dos Santos Filho <carlosfilho.canal@icloud.com>
2023-10-06 20:25:44 +0100

    New file conditions

--- Conditions.playground/Contents.swift
+++ Conditions.playground/Contents.swift
@@ -1,1 +1,20 @@
 import UIKit
+
+// CONDICIONAIS if E else
+
+var product: String
+
+let company = "Google"
+
+if company == "Google" {
+    product = "Android"
+} else {
+    product = "iPhone"
+}
+
+// CONDICIONAIS USANDO OPERADORES TERNÁRIOS
+
+// EXPRESSÃO ? valor-true : valor-false
+
+product = company == "Google" ? "Android" : "iPhone"
+print(product)
