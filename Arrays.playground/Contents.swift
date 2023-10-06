fe97d39c3982ca4a729f861fa3abfe46b2166d18
Carlos Augusto dos Santos Filho <carlosfilho.canal@icloud.com>
2023-10-06 23:57:58 +0100

    add arrays

---
+++ Arrays.playground/Contents.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+// ARQUITETURA DE DADOS DO TIPO ARRAY
+
+let userNames: [String] = ["Carlos", "joao", "Rafael"] // declarando e especificando o tipo
+
+
+// DECLARANDO COMO "VAR" E LISTA VAZIA PARA MODIFICAR COMO VOCÊ QUISER
+
+var numbers: [Int] = []
+
+numbers.append(1) // usando a função ".append" para adicionar um elemento a minha lista
+
+numbers += [2, 3] // incrementando uma nova lista a sua lista antiga
