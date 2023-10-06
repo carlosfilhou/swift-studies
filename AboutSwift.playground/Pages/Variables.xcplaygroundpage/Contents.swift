4b8976e7b96442e1b2f03b99894bf84ceb3fbdda
Carlos Augusto dos Santos Filho <carlosfilho.canal@icloud.com>
2023-10-06 15:45:29 +0100

    first-commit

---
+++ AboutSwift.playground/Pages/Variables.xcplaygroundpage/Contents.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+var user = "Carlos" // aqui estou declarando uma variável no Swift, pode alterar o seu estado
+
+let pi = 3.14 // aqui estou declarando uma variável constante no Swift, não pode alterar o seu estado
+
+print(user) // aqui estou printando no Swift
+print(pi)
+
+/* Esse aqui é um exemplo 
+ de comentários multi linha */
