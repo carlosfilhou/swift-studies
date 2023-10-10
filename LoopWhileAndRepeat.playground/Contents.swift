f73d6d1750c0853c92efa8ba67350bc91ea4f615
Carlos Augusto dos Santos Filho <carlosfilho.canal@icloud.com>
2023-10-10 11:06:58 +0100

    add LoopWhileAndRepeat

---
+++ LoopWhileAndRepeat.playground/Contents.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+// LOOP WHILE
+
+var i = 1
+
+while i <= 10 {
+    print("hello world! 10x com while!")
+    i += 1
+}
+
+
+// COM O REPEAT ELE PRIMEIRO EXECUTA UMA FUNÇÃO E DEPOIS CHECA CONDIÇÃO
+
+var indice = 1
+
+repeat {
+    print("Hello World 5x com repeat!")
+    indice += 1
+} while (indice <= 5)
