79a6eec036aa70deff91b1f85181398db205b88e
Carlos Augusto dos Santos Filho <carlosfilho.canal@icloud.com>
2023-10-10 01:49:14 +0100

    doc manipulatingArrays

--- ManipulatingArrays.playground/Contents.swift
+++ ManipulatingArrays.playground/Contents.swift
@@ -2,31 +2,66 @@ import UIKit
 
 // MANIPULANDO ARRAYS
 
-let userNames: [String] = ["Carlos", "joao", "Rafael"] // declarando meu array
+var userNames: [String] = [] // declarando meu array [vazio]
 
-userNames[1] // acessando um índice específico
+userNames.append("Felipe") // adicionando um novo alemento a minha lista
 
-let name = userNames[1] // acessando e armazenando esse valor dentro de uma variável
-                        // ATENÇÃO SE VOCÊ TENTAR ACESSAR UM ÍNDICE QUE NÃO EXISTE, ELE DIRÁ QUE ESTÁ FORA DO RANGE
+userNames += ["Carlos", "João", "Rafael"] // incrementando uma nova lista a uma lista existente
 
-name // variável que está carregando o índice que você atribuiu a ela
+print("primeiro print: ", userNames) // printando a minha lista
+
+userNames[0...1] = ["LucasSubstituto", "DanSubstituto", "GabrielSubstituto"] // troca dos elementos através de slice
+
+print("print após troca de elementos: ", userNames) // printando a minha lista
+
+userNames.swapAt(0, 1)
+
+print("print após swap de índice 0 e 1: ", userNames) // printando a minha lista
+
+userNames[1] // acessando apenas para me retornar um índice específico
+
+userNames.insert("MatheusNovo", at: 2) // inserir em um índice específico
+
+print("print depois de adicionar um novo índice [2]: ", userNames) // printando a minha lista
+
+userNames.remove(at: 0) // removendo um índice específico
+
+let removed = userNames.remove(at: 0) // removendo e alocando um índice pra dentro de uma variável
+
+let removedLast = userNames.removeLast() // removendo o último elemento da lista
+
+print("print depois de remover alguns índices: ", userNames) // printando a minha lista
+
+let firstIndice = userNames[1] // acessando e armazenando esse índice dentro de uma variável
+
+firstIndice // acessando variável apenas para retornar o índice que eu armazenei dentro dela
+
+userNames.first // retorna o pirmeiro índice
 
 
 
-// ACESSANDO OS ELEMENTOS ATRAVÉS DE FATIAS/SLICES
+userNames.isEmpty // função de condição para VALIDAÇÃO(if) se a lista está vazia (true ou false)
 
-let secondAndThree = userNames[1...2]
+userNames.count // função de condição para VALIDAÇÃO(if) tamanho da lista (número de indíces)
 
-
-
-// COLOCAR OS DOIS ELEMENTOS EM UMA NOVA LISTA E COMEÇAR A ACESSÁ-LOS A PARTIR DELA
-
-var firstTwo = Array(userNames[0...1])
-
-print(firstTwo)
+userNames.contains("Maria") // função de condição para VALIDAÇÃO(if) se contém o elemento espeficado
 
 
 
-// REMOVER TODOS OS ELEMENTOS DA LISTA
+let FirstAndSecod = userNames[0...1] // ACESSANDO OS ELEMENTOS ATRAVÉS DE FATIAS/SLICES
 
-firstTwo.removeAll()
+var firstTwo = Array(userNames[0...1]) // COLOCAR OS DOIS ELEMENTOS EM UMA NOVA LISTA E COMEÇAR A ACESSÁ-LOS A PARTIR DELA
+
+print("pritando os dois primeiros índices: ", firstTwo) // printando a nova lista
+
+userNames.removeAll() // Removendo todos os elementos da lista
+
+if let indice1 = userNames.first { // só irá executar se ele encontrar o elemento dentro do índice
+    print(indice1)
+}
+
+print("printando depois de usar a função removeAll: ", userNames) // printando a primeita lista novamente
+
+
+
+// ATENÇÃO SE VOCÊ TENTAR ACESSAR UM ÍNDICE QUE NÃO EXISTE, ELE DIRÁ QUE ESTÁ FORA DO RANGE
