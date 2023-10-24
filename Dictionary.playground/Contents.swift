fd54b8cf07458a2aee8f28c94b695767e68d4f2c
Carlos Augusto dos Santos Filho <carlosfilho.canal@icloud.com>
2023-10-25 00:45:16 +0100

    doc dictionary

--- Dictionary.playground/Contents.swift
+++ Dictionary.playground/Contents.swift
@@ -1,6 +1,6 @@
 import UIKit
 
-// DICTIONARY (ESTRUTURAS DESORDENADAS, ACESSAR CHAVE ATRAVÉS DO VALOR)
+// DICTIONARY (ESTRUTURAS DESORDENADAS, TRABALHA COM CHAVE E VALOR, PODE UTILIZAR QUALQUER TIPO DE DADO)
 
 
 var dicLojas: [String : Int] = [:] // inicializando dicionário e especificando que tipo ele será
@@ -9,8 +9,10 @@ dicLojas["Magazine"] = 200 // apenas adicionando elemento ao meu dicionário
 dicLojas["Americanas"] = 80
 dicLojas["Casas Bahia"] = 150
 
-print(dicLojas) // imprimindo dicionário
+dicLojas["Americanas"] = nil // removendo um elemento
 
+
+print(dicLojas) // imprimindo dicionário
 
 // inicializando dicionário e ja declarando produtos a ele
 var dicProducts: [Int:String] = [
@@ -25,7 +27,25 @@ print(dicProducts[4]) // imprimindo valor pela sua chave específica
 
 print(dicProducts[12] ?? "Nenhum valor encontrado") // adicionando um valor padrão caso não encontre nenhum valor dentro do índice especificado
 
+print(dicProducts.isEmpty) // verificando se o dicionário está vazio
+print(dicProducts.count) // contando quantos elementos meu dicionário possui
 
 
+// buscar todas as chaves (APENAS CHAVES)
+print("imprimindo apenas as chaves do dicionário: ")
+for key in dicProducts.keys {
+    print(key)
+}
+
+// buscar todas os valores (APENAS VALORES)
+print("Imprimindo apenas os valores do dicionário: ")
+for val in dicProducts.values {
+    print(val)
+}
 
 
+// buscar tanto a chave quanto o valor ao mesmo tempo
+print("imprimindo a chave e o valor ao mesmo tempo: ")
+for (key, value) in dicProducts {
+    print("chave: \(key) valor: \(value)")
+}
