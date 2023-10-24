d3a722d5a0544f147b707688bf5bc89f62242d0e
Carlos Augusto dos Santos Filho <carlosfilho.canal@icloud.com>
2023-10-25 00:55:38 +0100

    doc dictionary

--- Dictionary.playground/Contents.swift
+++ Dictionary.playground/Contents.swift
@@ -1,51 +1,50 @@
-import UIKit
+    import UIKit
 
-// DICTIONARY (ESTRUTURAS DESORDENADAS, TRABALHA COM CHAVE E VALOR, PODE UTILIZAR QUALQUER TIPO DE DADO)
-
-
-var dicLojas: [String : Int] = [:] // inicializando dicionário e especificando que tipo ele será
-
-dicLojas["Magazine"] = 200 // apenas adicionando elemento ao meu dicionário
-dicLojas["Americanas"] = 80
-dicLojas["Casas Bahia"] = 150
-
-dicLojas["Americanas"] = nil // removendo um elemento
+    // DICTIONARY (ESTRUTURAS DESORDENADAS, TRABALHA COM CHAVE E VALOR, PODE UTILIZAR QUALQUER TIPO DE DADO)
 
 
-print(dicLojas) // imprimindo dicionário
+    var dicLojas: [String : Int] = [:] // inicializando dicionário e especificando que tipo ele será
 
-// inicializando dicionário e ja declarando produtos a ele
-var dicProducts: [Int:String] = [
-    1 : "Cadeira",
-    2 : "Mesa",
-    3 : "Cama",
-    4 : "Armario",
-    5 : "Toalha"
-]
+    dicLojas["Magazine"] = 200 // apenas adicionando elemento ao meu dicionário
+    dicLojas["Americanas"] = 80
+    dicLojas["Casas Bahia"] = 150
 
-print(dicProducts[4]) // imprimindo valor pela sua chave específica
+    dicLojas["Americanas"] = nil // removendo um elemento
 
-print(dicProducts[12] ?? "Nenhum valor encontrado") // adicionando um valor padrão caso não encontre nenhum valor dentro do índice especificado
+    print(dicLojas) // imprimindo dicionário
 
-print(dicProducts.isEmpty) // verificando se o dicionário está vazio
-print(dicProducts.count) // contando quantos elementos meu dicionário possui
+    // inicializando dicionário e ja declarando produtos a ele
+    var dicProducts: [Int:String] = [
+        1 : "Cadeira",
+        2 : "Mesa",
+        3 : "Cama",
+        4 : "Armario",
+        5 : "Toalha"
+    ]
 
+    print(dicProducts[4]) // imprimindo valor pela sua chave específica
 
-// buscar todas as chaves (APENAS CHAVES)
-print("imprimindo apenas as chaves do dicionário: ")
-for key in dicProducts.keys {
-    print(key)
-}
+    print(dicProducts[12] ?? "Nenhum valor encontrado") // adicionando um valor padrão caso não encontre nenhum valor dentro do índice especificado
 
-// buscar todas os valores (APENAS VALORES)
-print("Imprimindo apenas os valores do dicionário: ")
-for val in dicProducts.values {
-    print(val)
-}
+    print(dicProducts.isEmpty) // verificando se o dicionário está vazio
+    print(dicProducts.count) // contando quantos elementos meu dicionário possui
 
 
-// buscar tanto a chave quanto o valor ao mesmo tempo
-print("imprimindo a chave e o valor ao mesmo tempo: ")
-for (key, value) in dicProducts {
-    print("chave: \(key) valor: \(value)")
-}
+    // buscar todas as chaves (APENAS CHAVES)
+    print("imprimindo apenas as chaves do dicionário: ")
+    for key in dicProducts.keys {
+        print(key)
+    }
+
+    // buscar todos os valo res (APENAS VALORES)
+    print("Imprimindo apenas os valores do dicionário: ")
+    for val in dicProducts.values {
+        print(val)
+    }
+
+
+    // buscar tanto a chave quanto o valor ao mesmo tempo
+    print("imprimindo a chave e o valor ao mesmo tempo: ")
+    for (key, value) in dicProducts {
+        print("chave: \(key) valor: \(value)")
+    }
