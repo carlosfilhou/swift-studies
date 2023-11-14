eb09644ed45142bccc8ee6f87ff0def3e66a97fe
Carlos Augusto dos Santos Filho <carlosfilho.canal@icloud.com>
2023-11-14 19:03:19 +0000

    doc functions

--- Functions.playground/Contents.swift
+++ Functions.playground/Contents.swift
@@ -1,3 +1,16 @@
 import UIKit
 
 // FUNCTIONS
+
+var numbers = 6 // declarando uma variável normal
+var result: set<Int> = [] // declarando uma variável do tipo Set
+
+while(numbers < 0) {
+    let generated = Int.random(in: 1...60) // gerar números aleatórios de 1 até 60
+    let res = result.insert(generated) // coleção de dados sem repetição
+    
+    if (res.inserted) {
+        numbers = numbers - 1
+        print(result)
+    }
+}
