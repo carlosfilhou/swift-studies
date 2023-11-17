1b98346141a5868d9c749437ad9106ce4676dcee
Carlos Augusto dos Santos Filho <carlosfilho.canal@icloud.com>
2023-11-17 17:40:35 +0000

    doc declaration functions

--- Functions.playground/Contents.swift
+++ Functions.playground/Contents.swift
@@ -2,15 +2,21 @@ import UIKit
 
 // FUNCTIONS
 
-var numbers = 6 // declarando uma variável normal
-var result: set<Int> = [] // declarando uma variável do tipo Set
+// GERADOR DA MEGA  SENA
 
-while(numbers < 0) {
-    let generated = Int.random(in: 1...60) // gerar números aleatórios de 1 até 60
-    let res = result.insert(generated) // coleção de dados sem repetição
+func megaSenaGenerator() { // apenas declarando a função (NÃO IRÁ EXECUTAR SE NÃO CHAMAR FUNÇÃO)
+    var numbers = 6 // declarando uma variável normal
+    var result: Set<Int> = [] // declarando uma variável do tipo Set
     
-    if (res.inserted) {
-        numbers = numbers - 1
-        print(result)
+    while(numbers > 0) {
+        let generated = Int.random(in: 1...60) // gerar números aleatórios de 1 até 60
+        let res = result.insert(generated) // colocando o resultado dentro de um SET para não haver números repitidos
+        
+        if (res.inserted) { // condição que faz um boolean
+            numbers = numbers - 1
+            print(result)
+        }
     }
 }
+
+megaSenaGenerator() // chamando a função
