0673d0db1c0a888315fdacb8f724876e1142132f
Carlos Augusto dos Santos Filho <carlosfilho.canal@icloud.com>
2023-11-21 02:14:38 +0000

    feat optional-ifLet

--- Optionals.playground/Contents.swift
+++ Optionals.playground/Contents.swift
@@ -14,7 +14,25 @@ print(emptyString)
 print(OptionalString)
 
 
-// TRAZENDO UM EXEMPLO REAL (VOCÊ IRÁ UTILIZAR VALORES NULOS QUANDO NÃO QUISER ESPECIFICAR ALGUM VALOR)
+// -----------------------------------------------
+
+
+// TRAZENDO UMA MANEIRA PARA RESOLVER (USANDO IF LET) - *Contador de String*
+
+var username: String? = "Carlos" // declarando como opcional para poder receber valores nulos
+username = nil
+
+if let username = username {
+    let chars = username.count // pegando a quantidade de caracteres
+    print("Quantidade de caracteres: \(chars)")
+} else {
+    print("O valor do nome está nulo, logo imprimir uma mensagem para o usuário")
+}
+
+// ----------------------------------------------
+
+
+// TRAZENDO OUTRO EXEMPLO REAL (PASSANDO UM VALOR PADRÃO CASO O VALOR SEJA NULO) - *Mega Sena Generator*
 
 func megaSenaGenerator4(total: Int?, maxNumber: Int) -> Set<Int> { // declarando a função com parâmetro (total está sendo declarada como opcional) e especificando o tipo do retorno
     var numbers = total ?? 6// declarando uma variável normal e determinando um valor padrão caso ela seja nula
