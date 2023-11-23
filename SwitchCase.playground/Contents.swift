a5ae84354fc233ba2ff2e2f8b380cc09f8c6e85b
Carlos Augusto dos Santos Filho <carlosfilho.canal@icloud.com>
2023-11-23 14:00:11 +0000

    feat finish-switchCase

--- SwitchCase.playground/Contents.swift
+++ SwitchCase.playground/Contents.swift
@@ -4,12 +4,35 @@ import UIKit
 
 // criando uma função que da um percentual de desconto
 func discount(productId: Double) -> Double {
-    switch(product) {
-    case 1:
-        return 0.2
+    switch(productId) {
+    case 0, 1: // aqui você pode adicionar 2 casos utilizando a vírgula para executar o bloco
+        return 0.2 // note que ele irá matar o bloco de código quando cair no return
     case 2:
         return 0.4
     default:
         return 0.0
     }
 }
+
+let d = discount(productId: 0)
+print("Desconto: \( Int(d * 100) )%")
+
+
+// criando uma função que imprime uma frase baseado no ID do produto
+func getCategory(productCategory: String) {
+    var message = ""
+    switch(productCategory) {
+    case "iPhone", "iPad", "Apple Watch": // aqui você pode adicionar 2 casos utilizando vírgula para executar o bloco
+        message = "Produto usando o Sistema Operacional da Apple"
+        break
+    case "Samsung", "Motorola":
+        message = "Produto usando o Sistema Operacional do Google, provavelmente o Android"
+        break
+    default:
+        message = "Produto não encontrado"
+        break
+    }
+    print(message)
+}
+
+getCategory(productCategory: "Samsung")
