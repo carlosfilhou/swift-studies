6ed917a50d58045f9d72bfea54371103b19009f9
Carlos Augusto dos Santos Filho <carlosfilho.canal@icloud.com>
2023-11-25 02:58:34 +0000

    doc struct-curiosity

--- Struct.playground/Contents.swift
+++ Struct.playground/Contents.swift
@@ -6,9 +6,15 @@ struct cars {
     var color = String()
     var eletric = Bool()
     var seats = Int()
+    var isOn = false // aqui você ja está atribuindo direto pq um carro sempre está desligado
+    
+    // sempre que criar um método que irá alterar um valor de uma propriedade, você precisa colocar o "mutating" na frente desse método
+    mutating func turnOn() {
+        isOn = true
+    }
 }
 
-// no struct diferente da class você atribuir a uma variável (instancia) sem precisar inicializar
-var mercedez = cars(color: "red", eletric: true)
-
+// no struct diferente da class você atribuir a uma variável (instancia) sem precisar inicializar. Mas você ainda pode inicializa-la se quiser
+var mercedez = cars(color: "red", eletric: true, seats: 4)
+mercedez.turnOn() // informando instancia (ou seja o objeto (carro) e chamando a função para liga-lo
 print(mercedez)
