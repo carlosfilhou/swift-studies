f12d92cafd57caf978f8b29a4ca213eb1470a91e
Carlos Augusto dos Santos Filho <carlosfilho.canal@icloud.com>
2023-11-25 02:36:54 +0000

    docs update-sintaxe

--- Struct.playground/Contents.swift
+++ Struct.playground/Contents.swift
@@ -3,12 +3,12 @@ import UIKit
 // STRUCT
 
 struct cars {
-    var color: String
-    var eletric: Bool
-    var seats: Int
+    var color = String()
+    var eletric = Bool()
+    var seats = Int()
 }
 
 // no struct diferente da class você atribuir a uma variável (instancia) sem precisar inicializar
-var mercedez = cars(color: "red", eletric: true, seats: 4)
+var mercedez = cars(color: "red", eletric: true)
 
 print(mercedez)
